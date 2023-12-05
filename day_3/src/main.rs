0064a6c1dd5194ae8352fbab8b9e6438a38ffe0e
Sina M. Omrani <sina.omrani@hotmail.com>
2023-12-05 17:35:15 +0330

    worst solution for part2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -12,7 +12,6 @@ fn main() {
         .iter()
         .map(|(i, l)| (i, re.find_iter(l)))
         .collect::<Vec<_>>();
-
     for (&i, m) in numbers {
         for matched in m.into_iter() {
             let begin_at_zero = if matched.start() == 0 { 0 } else { 1 };
@@ -21,7 +20,6 @@ fn main() {
             } else {
                 1
             };
-
             if (!not_symbol.contains(
                 &lines[i]
                     .1
@@ -56,4 +54,187 @@ fn main() {
         }
     }
     println!("{}", part_1);
+
+    // part 2
+    let mut part_2 = 0;
+    let all_gears = lines
+        .clone()
+        .into_iter()
+        .map(|(line_number, line)| {
+            (
+                line_number,
+                line.trim()
+                    .chars()
+                    .enumerate()
+                    .filter(|(_i, c)| c == &'*')
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    for (line_number, g) in all_gears {
+        for (index, _g) in g {
+            let mut adj = vec![];
+
+            if index != 0
+                && lines[line_number]
+                    .1
+                    .chars()
+                    .nth(index - 1)
+                    .unwrap()
+                    .is_ascii_digit()
+            {
+                let tmp = re.find_iter(lines[line_number].1);
+                let nmb = tmp
+                    .into_iter()
+                    .filter(|m| m.end() == index)
+                    .map(|m| m.as_str())
+                    .collect::<Vec<_>>()[0]
+                    .parse::<i128>()
+                    .unwrap();
+                adj.push(nmb);
+            }
+            if index != 140
+                && lines[line_number]
+                    .1
+                    .chars()
+                    .nth(index + 1)
+                    .unwrap()
+                    .is_ascii_digit()
+            {
+                let tmp = re.find_iter(lines[line_number].1);
+                let nmb = tmp
+                    .into_iter()
+                    .filter(|m| m.start() == index + 1)
+                    .map(|m| m.as_str())
+                    .collect::<Vec<_>>()[0]
+                    .parse::<i128>()
+                    .unwrap();
+                adj.push(nmb);
+            }
+
+            if line_number != 0 {
+                if lines[line_number - 1]
+                    .1
+                    .chars()
+                    .nth(index)
+                    .unwrap()
+                    .is_ascii_digit()
+                {
+                    let tmp = re.find_iter(lines[line_number - 1].1);
+
+                    let nmb = tmp
+                        .into_iter()
+                        .filter(|m| index <= m.end() && index >= m.start())
+                        .map(|m| m.as_str())
+                        .collect::<Vec<_>>()[0]
+                        .parse::<i128>()
+                        .unwrap();
+                    adj.push(nmb);
+                } else {
+                    if index != 0
+                        && lines[line_number - 1]
+                            .1
+                            .chars()
+                            .nth(index - 1)
+                            .unwrap()
+                            .is_ascii_digit()
+                    {
+                        let tmp = re.find_iter(lines[line_number - 1].1);
+                        let nmb = tmp
+                            .into_iter()
+                            .filter(|m| m.end() == index)
+                            .map(|m| m.as_str())
+                            .collect::<Vec<_>>()[0]
+                            .parse::<i128>()
+                            .unwrap();
+                        adj.push(nmb);
+                    }
+
+                    if index != 140
+                        && lines[line_number - 1]
+                            .1
+                            .chars()
+                            .nth(index + 1)
+                            .unwrap()
+                            .is_ascii_digit()
+                    {
+                        let tmp = re.find_iter(lines[line_number - 1].1);
+                        let nmb = tmp
+                            .into_iter()
+                            .filter(|m| m.start() == index + 1)
+                            .map(|m| m.as_str())
+                            .collect::<Vec<_>>()[0]
+                            .parse::<i128>()
+                            .unwrap();
+                        adj.push(nmb);
+                    }
+                }
+            }
+
+            if line_number != lines.len() - 1 {
+                if lines[line_number + 1]
+                    .1
+                    .chars()
+                    .nth(index)
+                    .unwrap()
+                    .is_ascii_digit()
+                {
+                    let tmp = re.find_iter(lines[line_number + 1].1);
+                    let nmb = tmp
+                        .into_iter()
+                        .filter(|m| index <= m.end() && index >= m.start())
+                        .map(|m| m.as_str())
+                        .collect::<Vec<_>>()[0]
+                        .parse::<i128>()
+                        .unwrap();
+                    adj.push(nmb);
+                } else {
+                    if index != 0
+                        && lines[line_number + 1]
+                            .1
+                            .chars()
+                            .nth(index - 1)
+                            .unwrap()
+                            .is_ascii_digit()
+                    {
+                        let tmp = re.find_iter(lines[line_number + 1].1);
+                        let nmb = tmp
+                            .into_iter()
+                            .filter(|m| m.end() == index)
+                            .map(|m| m.as_str())
+                            .collect::<Vec<_>>()[0]
+                            .parse::<i128>()
+                            .unwrap();
+                        adj.push(nmb);
+                    }
+
+                    if index != 140
+                        && lines[line_number + 1]
+                            .1
+                            .chars()
+                            .nth(index + 1)
+                            .unwrap()
+                            .is_ascii_digit()
+                    {
+                        let tmp = re.find_iter(lines[line_number + 1].1);
+                        let nmb = tmp
+                            .into_iter()
+                            .filter(|m| m.start() == index + 1)
+                            .map(|m| m.as_str())
+                            .collect::<Vec<_>>()[0]
+                            .parse::<i128>()
+                            .unwrap();
+                        adj.push(nmb);
+                    }
+                }
+            }
+
+            if adj.len() == 2 {
+                part_2 += adj[0] * adj[1];
+            }
+        }
+    }
+
+    println!("{part_2}");
 }
